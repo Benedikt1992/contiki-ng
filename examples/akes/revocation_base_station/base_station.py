45043b78468c8dc6320596a36c3a638d36c110ff
Benedikt Bock <mail@benedikt1992.de>
2018-07-11 18:47:52 +0200

    sending bits with basestation

--- examples/akes/revocation_base_station/base_station.py
+++ examples/akes/revocation_base_station/base_station.py
@@ -7,19 +7,19 @@ from base_station.logging import setup_logging
 
 logger = logging.getLogger(name='base_station')
 
-control_byte_default = b'\x00'
-control_byte_terminate = b'\x02'
+control_byte_default = '\x00'
+control_byte_terminate = '\x02'
 
 revocation_list = []
 
 def MAC_to_bytearray(mac_addr):
-    result = b''
+    result = ''
     for group in mac_addr.split('.'):
-        result += bytes.fromhex(group)
+        result += bytes.fromhex(group).decode('utf-8')
     return result
 
 def build_payload(control_byte, revoke_node, dst_node_addrs):
-    payload = b''
+    payload = ''
     payload += control_byte
     payload += revoke_node
     for addr in dst_node_addrs:
@@ -39,8 +39,9 @@ class BaseStation:
                                     MAC_to_bytearray('0200.0000.0000.0000'),
                                     [MAC_to_bytearray('0100.0000.0000.0000')]
                                    )
+            print(payload)
             #payload.decode('ascii')
-            response = client.post(CONFIG['path'], payload.decode('ascii'), timeout=None)
+            response = client.post(CONFIG['path'], payload, timeout=None)
 
             print( response.pretty_print())
 
